fb0ae434bb860b0b003501f0f45cbe48c4e3e716
Shivam Khandelwal <51732876+ShivamK30@users.noreply.github.com>
2021-07-29 15:19:17 +0530

    Update ChessBoardRenderer.java

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,6 +2,13 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
+		if(square<8 || (square>15 && square<24) || (square>31 && square<40) || (square>47 && square<56)) {
+			if((square)%2==0)
+				return false;
+		}
+		else
+			if((square)%2==1)
+				return false;
 		System.out.println(square);
 		return true;
 	}
